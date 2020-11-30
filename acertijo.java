7e5dfed75e5493b3fc5ffa888e8e6492785aba24
Kryss26 <yosoycris2010@gmail.com>
2020-11-30 19:30:48 +0100

    Acertijo creado en Java

---
+++ acertijo.java
@@ -0,0 +1,22 @@
+public static void main(String[] args) {
+    char answer;
+    System.out.println("Tengo ojos pero no veo, agua pero no bebo y barba pero no me afeito, ¿Quién soy?"); 
+    System.out.println("(--------------------------)");
+    System.out.println("a) Pez manta");
+    System.out.println("b) Coco");
+    System.out.println("c) Caqui");
+    System.out.println("d) Melocotón")
+    
+    while (answer=b){
+
+    switch (answer) {
+        case a:
+            System.out.println("¡Incorrecto!");
+            break;
+        case b:
+            System.out.println("¡Correcto!");
+        default:
+            break;
+    }
+}
+}
